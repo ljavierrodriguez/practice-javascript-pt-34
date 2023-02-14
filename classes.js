6ea15c28e812c967b8cc0970bb9609791ae21f13
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2023-02-14 00:25:28 +0000

    loops, oop, dom y eventos

---
+++ classes.js
@@ -0,0 +1,15 @@
+class Persona {
+    constructor(name, lastname){
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    saludo(){
+        return `${this.name} ${this.lastname}`;
+    }
+}
+
+
+/* module.exports = {
+    Persona
+} */
---
+++ example.js
@@ -0,0 +1,8 @@
+/* const { Persona } = require('./classes'); */
+
+class Estudiante extends Persona {
+
+}
+
+let e = new Estudiante("Luis", "Rodriguez");
+console.log(e);
